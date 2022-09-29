872882ff52ab067ee8e2efefdf2cb7ef1509d8e7
sascha10000 <github@duepre-sa.de>
2022-09-30 00:05:38 +0200

    error handling shortened

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,10 @@ async fn main() {
     let example1 = warp::get()
         .and(warp::path("api"))
         .and(warp::path("get-prerender"))
-        .map(|| Response::builder().body(prerender()));
+        .map(|| Response::builder().body( match prerender() {
+            Ok(it) => it,
+            Err(err) => err.to_string().to_owned(),
+        }));
 
     let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
     let port: u16 = match env::var(port_key) {
@@ -20,21 +23,12 @@ async fn main() {
     warp::serve(example1).run((Ipv4Addr::LOCALHOST, port)).await
 }
 
-fn prerender() -> String {
+fn prerender() -> Result<String, anyhow::Error>  {
     let options = LaunchOptions::default_builder()
         .build()
         .expect("Couldn't find appropriate Chrome binary.");
-    let browser = match Browser::new(options) {
-        Ok(it) => it,
-        Err(err) => return err.to_string().to_owned(),
-    };
-    let tab = match browser.wait_for_initial_tab() {
-        Ok(it) => it,
-        Err(err) => return err.to_string().to_owned(),
-    };
-    let content = match tab.navigate_to("https://www.sotrusty.com/").unwrap().wait_until_navigated().unwrap().get_content() {
-        Ok(it) => it,
-        Err(err) => return err.to_string().to_owned(),
-    };
-    return content;
+    let browser = Browser::new(options)?;
+    let tab = browser.wait_for_initial_tab()?;
+    let content = tab.navigate_to("https://www.sotrusty.com/").unwrap().wait_until_navigated().unwrap().get_content()?;
+    return Ok(content);
 }
