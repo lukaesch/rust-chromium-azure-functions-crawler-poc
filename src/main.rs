d50f4e73d167fb382033c67e93456b993d21e97b
Lukas Schmyrczyk <lukas.schmyrczyk@lionmint.com>
2023-01-08 20:37:43 +0100

    Clean up

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use std::{env, net::Ipv4Addr, time::Duration};
-use warp::{http::Response, Filter};
 use headless_chrome::{Browser, LaunchOptions};
+use std::{env, net::Ipv4Addr};
+use warp::{http::Response, Filter};
 
 struct Config {
     port: u16,
@@ -10,21 +10,26 @@ struct Config {
 #[tokio::main]
 async fn main() -> () {
     let config = load_config();
-    println!("Starting server on port {}", config.port);
     let app = warp::get()
         .and(warp::path!("api" / "prerender" / String))
         .map(move |path| render_path(path, "html".to_owned(), false, config.is_headless))
         .or(
-            warp::path!("api" / "prerender" / String / String / bool)
-                .map(move |param, element, is_http| render_path(param, element, is_http, config.is_headless)),
+            warp::path!("api" / "prerender" / String / String / bool).map(
+                move |url, element, is_http| render_path(url, element, is_http, config.is_headless),
+            ),
         );
-    warp::serve(app).run((Ipv4Addr::LOCALHOST, config.port)).await
+    warp::serve(app)
+        .run((Ipv4Addr::LOCALHOST, config.port))
+        .await;
+    println!("Server started on port {}", config.port)
 }
 
 fn load_config() -> Config {
     let headless_key = "FUNCTIONS_HEADLESS_MODE";
     let is_headless: bool = match env::var(headless_key) {
-        Ok(val) => val.parse().expect("Headless mode not set. Defaulting to true"),
+        Ok(val) => val
+            .parse()
+            .expect("Headless mode not set. Defaulting to true"),
         Err(_) => true,
     };
     let port_key = "FUNCTIONS_CUSTOMHANDLER_PORT";
@@ -49,7 +54,12 @@ fn render_path(
     };
 }
 
-fn prerender(path: String, element: String, is_http: bool, is_headless: bool) -> Result<String, anyhow::Error> {
+fn prerender(
+    path: String,
+    element: String,
+    is_http: bool,
+    is_headless: bool,
+) -> Result<String, anyhow::Error> {
     let options = LaunchOptions::default_builder()
         .headless(is_headless)
         .build()
@@ -57,12 +67,12 @@ fn prerender(path: String, element: String, is_http: bool, is_headless: bool) ->
 
     let url = &(if is_http { "http://" } else { "https://" }.to_owned() + path.as_str());
     println!("Fetching {}", &url);
-
-    let content = Browser::new(options)?
-        .wait_for_initial_tab()?
-        .navigate_to(&url)?
-        .wait_until_navigated()?
-        .wait_for_element_with_custom_timeout(&element, Duration::from_secs(10))?
-        .get_content();
-    Ok(content?)
+    let browser = Browser::new(options)?;
+    let tab = &browser.wait_for_initial_tab()?;
+    tab.navigate_to(&url).expect("failed to navigate");
+    println!("Waiting for element {}", &element);
+    tab.wait_for_element(&element).expect("failed to wait for element");
+    let content = &tab.get_content()?;
+    println!("Done. Content size: {}kb", content.len() / 1024);
+    Ok(content.to_string())
 }
